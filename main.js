655dc9669a6d2baf8c7be2d9c1de87299edbfb40
DESKTOP-GOMHKLA\migue <mgomez@albany.edu>
2017-03-11 22:57:45 -0500

    Quote commit

---
+++ main.js
@@ -0,0 +1,10 @@
+var quotes = [
+    'First quote',
+    'Second Quote'
+]
+
+function newQuote(){
+    var randomNumber = Math.floor(Math.random) * (quotes.length));
+    document.getElementById('quoteDisplay').innerHTML = quotes[randomNumber];
+    
+}
